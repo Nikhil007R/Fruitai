3e0a3212c53239d60d7f7f8fa0bcaa6e49e999ef
Nikhil Rawat <nikhilrawaa311@gmail.com>
2024-09-15 21:10:09 +0530

    Home page created

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,28 +1,54 @@
-// Home.js
 import React from "react";
-import { Link } from "react-router-dom";
-import "../App.css";
+import { Link } from "react-router-dom";  // Import Link from react-router-dom
+import styles from './Home.module.css'; // Import the CSS module
+import '../App.css'
+import { MdGTranslate } from "react-icons/md";
 
-function Home() {
+const Home = () => {
   return (
-    <div className="home-container">
-      <h1>Fruit.AI</h1>
-      <div className="services">
-        <div>
-          <Link to="/chatbot">Chatbot</Link>
+    <div className={styles.homeContainer}>
+      <div className={styles.header}>
+        <h1>Fruit.Ai</h1>
+        <p>"Be Healthy!"</p>
+      </div>
+
+      <div className={styles.buttonGrid} >
+        {/* Link to Chatbot */}
+        <div className={`${styles.button} ${styles.chat}`}>
+          <Link to="/chatbot" className={`${styles.buttonLink}`} style={{color: "#C84AD3"}}>
+            Chat.
+          </Link>
         </div>
-        <div>
-          <Link to="/translator">Translator</Link>
+
+        {/* Link to Translator */}
+        <div className={`${styles.button} ${styles.translate}`}>
+          <Link to="/translator" className={styles.buttonLink}>
+            <MdGTranslate className={styles.image}/>
+          </Link>
         </div>
-        <div>
-          <Link to="/faq">FAQ</Link>
+
+        {/* Link to FAQ */}
+        <div className={`${styles.button} ${styles.faqs} `}>
+          <Link to="/faq" className={`${styles.buttonLink} faqs`}>
+            FAQs
+          </Link>
         </div>
-        <div>
-          <Link to="/about">About</Link>
-        </div>
+
+        {/* Link to About */}
+        <div className={`${styles.button} ${styles.about} `}>
+          <Link to="/about" style={{color: "#C84AD3"}} className={styles.buttonLink}>
+            About
+          </Link>
+        </div>  
+      </div>
+
+      <div className={styles.dots}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </div>
   );
-}
+};
 
 export default Home;
